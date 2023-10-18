ab052749be9fe640c42ef140060c03c1273c446d
barleymilk <barleymilk640@gmail.com>
2023-10-18 10:39:28 +0900

    nothing changed

--- app/models.py
+++ app/models.py
@@ -1,8 +1,11 @@
 import sys, os
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
-from sqlalchemy import Column, INTEGER, String, TIMESTAMP
+from sqlalchemy import Column, INTEGER, String, TIMESTAMP, LargeBinary, ForeignKey
+from sqlalchemy.dialects.postgresql import JSON
+from sqlalchemy.orm import relationship
 
 from database import Base
+
 
 class EmailCodeCheck(Base):
     __tablename__ = "email_code_check"
@@ -12,13 +15,32 @@ class EmailCodeCheck(Base):
     creation_time = Column(TIMESTAMP, nullable=False)
     expiration_time = Column(TIMESTAMP, nullable=False)
 
+
 class Consultant(Base):
-    __tablename__ = "consultant"
+    __tablename__ = 'consultant'
+
     id = Column(INTEGER, primary_key=True)
-    email = Column(String(255), nullable=False)
-    hashedpassword = Column(String(255), nullable=False)
-    name = Column(String(255), nullable=False)
-    phone = Column(String(255), nullable=False)
-    birthday = Column(String(255), nullable=False)
-    status = Column(String(255), nullable=False)
-    extension = Column(String(255), nullable=False)
+    email = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    name = Column(String, nullable=False)
+    phone = Column(String, nullable=False)
+    birthday = Column(String, nullable=False)
+    status = Column(String, nullable=False, default='대기')
+    extension = Column(String, nullable=False, default='0000')
+
+    customer = relationship("Customer", back_populates="consultant")
+    
+
+class Customer(Base):
+    __tablename__ = 'customer'
+
+    id = Column(INTEGER, primary_key=True)
+    consultant_id = Column(INTEGER, ForeignKey("consultant.id"))
+    name = Column(String, nullable=False)
+    phone = Column(String, nullable=False)
+    birthday = Column(String, nullable=True)
+    email = Column(String, nullable=True)
+    gender = Column(String, nullable=True)
+    memo = Column(String, nullable=True)
+
+    consultant = relationship("Consultant", back_populates="customer")
