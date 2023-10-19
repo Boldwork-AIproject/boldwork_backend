05036d25a1a16ccae209baba119d3330a6563a3e
barleymilk <barleymilk640@gmail.com>
2023-10-20 00:57:57 +0900

    Move checking token codes router/upload.py to funcs/check_token.py

---
+++ app/funcs/check_token.py
@@ -0,0 +1,17 @@
+from fastapi import HTTPException, Request
+from starlette import status
+from jose import JWTError
+
+from jwt_utils import verify_access_token
+
+
+def get_current_user(request: Request):
+    try:
+        token = request.cookies.get("access_token")
+        data = verify_access_token(token)
+        return data
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Invalid token"
+        )
