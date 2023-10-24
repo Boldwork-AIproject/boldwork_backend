0fdf977f55f277dad26971dfd3c9e42da3ceb27f
SHIN.sped <sml2031@gmail.com>
2023-10-24 19:56:33 +0900

    update info

--- app/routers/info.py
+++ app/routers/info.py
@@ -3,7 +3,7 @@ from fastapi.exceptions import HTTPException
 from fastapi.responses import JSONResponse
 
 from app.models import Customer, User
-
+from typing import List, int
 router = APIRouter()
 
 # `user` 매개변수는 `User` 타입입니다.
@@ -20,7 +20,7 @@ async def get_customers(
         raise HTTPException(status_code=401, detail="관리자만 접근할 수 있습니다.")
 
     # 고객 목록 조회
-    customers = await Customer.filter(  # `customers` 변수는 `list` 타입입니다.
+    customers: List[Customer] = await Customer.filter(
         author=user,  # `user` 매개변수는 `User` 타입입니다.
         name=name,  # `name` 매개변수는 `str` 타입입니다.
         search=search,  # `search` 매개변수는 `str` 타입입니다.
@@ -34,7 +34,7 @@ async def get_customers(
         "size": customers.per_page,  # `size` 속성은 `int` 타입입니다.
         "totalPage": customers.pages,
         "totalCount": customers.total,
-        "data": customers.items,  # `items` 속성은 `list` 타입입니다.
+        "data": customers,  # `items` 속성은 `list` 타입입니다.
     })
 
 # `customer_id` 매개변수는 `int` 타입입니다.
@@ -50,7 +50,7 @@ async def get_customer(
         raise HTTPException(status_code=401, detail="관리자만 접근할 수 있습니다.")
 
     # 고객 정보 조회
-    customer = await Customer.get(customer_id)  # `customer` 변수는 `Customer` 타입입니다.
+    customer: Customer = await Customer.get(customer_id)  # `customer` 변수는 `Customer` 타입입니다.
 
     # 고객이 존재하지 않으면 오류
     if not customer:
