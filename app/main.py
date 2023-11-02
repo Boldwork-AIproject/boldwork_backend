f58f81a6113fd0849534a36bf75aefeab0d41b56
barleymilk <barleymilk640@gmail.com>
2023-11-02 10:45:12 +0900

    Add CORSMiddleware

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ import sys, os
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 from fastapi import FastAPI
 from starlette import status
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routers import login, logout, upload, signup, inference, check, info, mypage
 
@@ -14,6 +15,19 @@ app.include_router(inference.router)
 app.include_router(check.router)
 app.include_router(info.router)
 app.include_router(mypage.router)
+
+origins = [
+    "http://localhost:3000",
+    "localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 # 메인페이지
 @app.get("/", status_code=status.HTTP_200_OK)
